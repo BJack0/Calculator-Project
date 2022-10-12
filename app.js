7986bc52b3b4e6f0368b977f8dde78ede1c5e015
Brandon <BJack0@outlook.com>
2022-10-12 15:59:12 +0100

    Create and link JS and CSS

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Welcome to the Calculator Project console!")
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function subtract(num1, num2) {
+  return num1 - num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function divide(num1, num2) {
+  return num1 / num2
+}
+
+var operator = add
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2)
+}
