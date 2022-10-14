d0b7816a70d44f1810dc3c0d92055944ad6804ff
Brandon <BJack0@outlook.com>
2022-10-14 11:30:17 +0100

    Create functional display and buttons

--- app.js
+++ app.js
@@ -21,3 +21,60 @@ var operator = add
 function operate(operator, num1, num2) {
   return operator(num1, num2)
 }
+
+var displayValue = document.querySelector("#display p")
+
+const buttonClear = document.getElementById("clear")
+buttonClear.addEventListener("click", () => {
+  displayValue.textContent = ""
+})
+
+const button0 = document.getElementById("button0")
+button0.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "0"
+})
+
+const button1 = document.getElementById("button1")
+button1.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "1"
+})
+
+const button2 = document.getElementById("button2")
+button2.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "2"
+})
+
+const button3 = document.getElementById("button3")
+button3.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "3"
+})
+
+const button4 = document.getElementById("button4")
+button4.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "4"
+})
+
+const button5 = document.getElementById("button5")
+button5.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "5"
+})
+
+const button6 = document.getElementById("button6")
+button6.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "6"
+})
+
+const button7 = document.getElementById("button7")
+button7.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "7"
+})
+
+const button8 = document.getElementById("button8")
+button8.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "8"
+})
+
+const button9 = document.getElementById("button9")
+button9.addEventListener("click", () => {
+  displayValue.textContent = displayValue.textContent + "9"
+})
